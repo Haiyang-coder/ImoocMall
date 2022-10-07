d206b729db5cb3e86f83f225ecdb00e5b9f156d0
Haiyang <shk4176@163.com>
2022-10-07 20:01:07 +0800

    新建SpringBoot项目

---
+++ imoocMall/src/test/java/com/shk/mall/imoocmall/ImoocMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shk.mall.imoocmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImoocMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
